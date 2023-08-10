2b08116e1c220a5cc9ef55cbe6284934c72f14af
votaon <117756605+votaon@users.noreply.github.com>
2023-08-10 15:36:07 +0200

    Add files via upload

--- Parking.cz/Prototype/main.py
+++ Parking.cz/Prototype/main.py
@@ -1,9 +1,7 @@
-#v souboru: ID místa, Název místa, souřadnice místa, celkem míst, obsazených míst
-#operace: obsadit místo, uvolnit místo, celkem míst, celkem obsazených
 #in text file: ID | Name of teh place | Coordinates | All spots | Free spots
 
+#shows ID and name of place
 def list_places():
-    #projde seznam míst a vyplivne pole ID a název
     places=open("places.txt","r")
     nation=""
     for line in places:
@@ -20,45 +18,46 @@ def list_places():
         nation+="\n"
     places.close()
     return nation
+
+#shows free spots of ID
 def get_place_free_spots(ID):
-    #vrací počet aktuálně volných míst u místa ID
     places=open("places.txt","r")
     a=1
     i=0
     for line in places:
-        if a==int(ID):
+        if str(a)==str(ID):
             for char in line:
                 if char == "|":
                     i +=1
                 if i == 4:
                     if char != "|" and char != "\n":
-                        print("Free spots:", char)
+                        print("Free spots:",char)
         a+=1
     places.close()
 
+#shows total spots of ID
 def get_place_total_spots(ID):
-    #vrací celkový počet míst na místě ID
     places=open("places.txt","r")
     a=1
     i=0
     for line in places:
-        if a==int(ID):
+        if str(a)==str(ID):
             for char in line:
                 if char == "|":
                     i +=1
                 if i == 3:
                     if char != "|" and char != "\n":
-                        print("Total spots:", char)
+                        print("Total spots",char)
         a+=1
     places.close()
 
+#basically first function but doesn't show ID
 def get_place_name(ID):
-    #vrací jméno místa ID
     places=open("places.txt","r")
     nation=""
     a=1
     for line in places:
-        if a==int(ID):
+        if str(a)==str(ID):
             text=""
             i=0
             for char in line:
@@ -71,15 +70,14 @@ def get_place_name(ID):
         a+=1
     places.close()
     return nation
-
+#shows coordinates of ID
 def get_place_coordinates(ID):
-    #vrací souřadnice místa ID
     places=open("places.txt","r")
     a=1
     i=0
     text=""
     for line in places:
-        if a ==ID:
+        if str(a) ==str(ID):
             for char in line:
                 if char == "|":
                     i +=1
@@ -89,17 +87,15 @@ def get_place_coordinates(ID):
         a+=1
     places.close()
     return text
-
+#subtracts 1 from free spots
 def enter_place(ID):
-    #zapíše pro ID o jeden víc obsazených(a kontrola jestli je volné místo)
     places=open("places.txt","r")
-    text2=""
     a=1
     i=0
-    text=""
     text2=""
     for line in places:
-        if a ==ID:
+        text=""
+        if str(a) ==str(ID):
             for char in line:
                 if char == "|":
                     i +=1
@@ -110,9 +106,9 @@ def enter_place(ID):
                         spots=int(char)
                         if spots > 0:
                             spots -=1
-                            text+=str(spots)
                         else:
-                            text2="Spots are already full."
+                            print("Spots are already full.")
+                        text+=str(spots)
                 else:
                     text+=char
         else:
@@ -122,17 +118,16 @@ def enter_place(ID):
     places.close()
     places = open("places.txt","w")
     places.write(text2)
-    return False
 
+#adds 1 to free spots
 def leave_place(ID):
     places=open("places.txt","r")
-    text2=""
     a=1
     i=0
-    text=""
     text2=""
     for line in places:
-        if a ==ID:
+        text=""
+        if str(a) ==str(ID):
             for char in line:
                 if char == "|":
                     i +=1
@@ -141,11 +136,11 @@ def leave_place(ID):
                         text+=char
                     else:
                         spots=int(char)
-                        if spots < numa:
-                            spots +=1
-                            text+=str(spots)
+                        if spots >= numa:
+                            print("Spots are already empty.")
                         else:
-                            text2="Spots are already empty."
+                            spots +=1
+                        text+=str(spots)
                 elif i == 3:
                     if char == "|" or char =="\n":
                         text+=char
@@ -153,7 +148,7 @@ def leave_place(ID):
                         numa=int(char)
                         text+=char
                 else:
-                    text+=char
+                    text+=char        
         else:
             text=line
         text2+=text
@@ -161,7 +156,67 @@ def leave_place(ID):
     places.close()
     places = open("places.txt","w")
     places.write(text2)
-    return 0
-    #zapíše pro ID o jeden míň(s kontrolou)
-    return False
-print ("MENU\nTo list places type 1\nTo get free spots of place tzpe 2\n")
+def definer(kuk2):
+    if kuk2 == "spodni brno bohunice" or kuk2 == "1":
+        kuk2 = "1"
+    elif kuk2 == "parkoviste penzion pro duchodce stary liskovec" or kuk2 =="2":
+        kuk2 =="2"
+    elif kuk2 == "parkoviste mikulaskovo namesti" or kuk2 == "3":
+        kuk2 ="3"
+    return kuk2
+import os
+
+while True:
+    kuk = str(input("MENU\nTo get free spots of place type 1\nTo get all spots of place type 2\nTo get coordinations of places type 3\nTo enter place type 4\nTo leave place type 5\nTo exit type L:"))
+    if kuk=="l":
+        exit()
+    else:
+        print("ID|Name of place\n" + list_places())
+        if kuk == "1":
+            kuk2 = str(input("Type ID or name of place(without punctuation):").lower())
+            kuk2=definer(kuk2)
+            if kuk2 == "1" or kuk2 == "2" or kuk2 == "3":
+                get_place_free_spots(kuk2)
+                continue
+            else :
+                print("Nonexistent ID or name")
+                continue
+        elif kuk == "2":
+            kuk2 = str(input("Type ID or name of place(without punctuation):").lower())
+            kuk2=definer(kuk2)
+            if kuk2 == "1" or kuk2 == "2" or kuk2 == "3":
+                get_place_total_spots(kuk2)
+                continue
+            else:
+                print("Nonexistent ID or name")
+                continue
+        elif kuk == "3":
+            kuk2 = str(input("Type ID or name of place(without punctuation):").lower())
+            kuk2=definer(kuk2)
+            if kuk2 == "1" or kuk2 == "2" or kuk2 == "3":
+                print("Coordinates:",get_place_coordinates(kuk2))
+                continue
+            else :
+                print("Nonexistent ID or name")
+                continue
+        elif kuk == "4":
+            kuk2 = str(input("Type ID or name of place(without punctuation):").lower())
+            kuk2=definer(kuk2)
+            if kuk2 == "1" or kuk2 == "2" or kuk2 == "3":            
+                enter_place(kuk2)
+                continue
+            else:
+                print("Nonexistent ID or name")
+                continue
+        elif kuk == "5":
+            kuk2 = str(input("Type ID or name of place(without punctuation):").lower())
+            kuk2=definer(kuk2)
+            if kuk2 == "1" or kuk2 == "2" or kuk2 == "3":
+                leave_place(kuk2)
+                continue
+            else :
+                print("Nonexistent ID or name")
+                continue
+        else:
+            print("Wrong input")
+            continue
